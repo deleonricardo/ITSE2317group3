26c0dc76825b5673510b5e2bdb7c5c8cb03f6a76
AndresCortez43 <acortez65129@mymail.tstc.edu>
2020-11-22 20:26:08 -0600

    User Login Test
    I have tested the user login screen with a simple users table that has only what needs to be tested (ID and Password).
    Test partially successful (See comments in UserLogin.cs)

--- SelfCheckoutGroupProject/SelfCheckoutGroupProject/UserLogin.cs
+++ SelfCheckoutGroupProject/SelfCheckoutGroupProject/UserLogin.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
 namespace SelfCheckoutGroupProject
 {
     public partial class UserLogin : Form
@@ -23,23 +27,54 @@ namespace SelfCheckoutGroupProject
             {
                 //need database to pull instance of Manager info?? RDL
                 //check password
-                if ((txtID.Text != frmMain.nManager.sMIDNum && txtPassword.Text != frmMain.nManager.sMPass) || (txtID.Text != frmMain.nEmployee.sEIDNum && txtPassword.Text != frmMain.nEmployee.sEPass))
-                {
-                    //error message and clear text boxes to try again
-                    MessageBox.Show("Error, Incorrect Entry");
-                    txtID.Text = "";
-                    txtPassword.Text = "";
-                }
-                else
-                {
 
-                //open user input screen - RDL
+                //Connection has been made to the users table. Only has 2 columns: userId and userPassword for testing
+                //Test is partially successful. Can get to the  user Interface form by entering the correct login information
+
+                //ERRORS: Can still access the User Interface Form by putting in random / Incorrect login info
+                //Maybe move the IF Statement made by RDL to help test for valid information? - Andres.
+
+                string userTest = "server=cstnt.tstc.edu;user=group3;database=group3;port=3306;password=password3";
+                MySqlConnection Conn  = new MySqlConnection(userTest);
+
+                string selectStatement = "SELECT * FROM group3.users WHERE userID = '" + txtID.Text.Trim() + "'and userPassword = '" + txtPassword.Text.Trim()+"'";
+                MySqlCommand selectComm = new MySqlCommand(selectStatement, Conn);
+                MySqlDataAdapter userDA = new MySqlDataAdapter(selectStatement, Conn);
+                DataSet userTable = new DataSet();
+                
+                userDA.Fill(userTable, "users");
+
+                if (userTable.Tables.Count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
                 UserInterface InterfaceScreen = new UserInterface();
                 InterfaceScreen.Show();
                 this.Hide();
+                
                 }
 
-                }
+
+
+
+
+
+				if ((txtID.Text != frmMain.nManager.sMIDNum && txtPassword.Text != frmMain.nManager.sMPass) || (txtID.Text != frmMain.nEmployee.sEIDNum && txtPassword.Text != frmMain.nEmployee.sEPass))
+				{
+					//error message and clear text boxes to try again
+					MessageBox.Show("Error, Incorrect Entry");
+					txtID.Text = "";
+					txtPassword.Text = "";
+				}
+				else
+				{
+
+					//open user input screen - RDL
+					UserInterface InterfaceScreen = new UserInterface();
+					InterfaceScreen.Show();
+					this.Hide();
+				}
+
+			}
             catch(Exception c)
             {
                 MessageBox.Show(c.Message);
