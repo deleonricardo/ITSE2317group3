56dd3106d0fa718e25ec4b10faf1911b646c328c
AndresCortez43 <acortez65129@mymail.tstc.edu>
2020-12-08 11:50:13 -0600

    User Interface Update
    Update user Interface labels

--- SelfCheckoutGroupProject/SelfCheckoutGroupProject/UserInterface.Designer.cs
+++ SelfCheckoutGroupProject/SelfCheckoutGroupProject/UserInterface.Designer.cs
@@ -75,6 +75,7 @@
 			this.btnDelete.TabIndex = 1;
 			this.btnDelete.Text = "Delete Item";
 			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
 			// 
 			// label1
 			// 
